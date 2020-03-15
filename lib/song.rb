31541b050a59097cbd201717c1f6f166da147654
Brian Vehige <brianvehige@gmail.com>
2020-03-15 21:23:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,28 +1,2 @@
 require 'pry'
 
-class Song
-  attr_accessor :name
-  attr_reader :artist
-  
-  extend 
-  Memorable::ClassMethods
-  extend
-  Findable
-  include
-  Memorable::InstanceMethods 
-  include Paramable 
-  
-  
-
-  @@songs = []
-
-
-  def self.all
-    @@songs
-  end
-
-  def artist=(artist)
-    @artist = artist
-  end
-
-end
